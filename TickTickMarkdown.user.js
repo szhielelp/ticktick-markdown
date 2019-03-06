d6db88f51e6eae3c0929a428c5fc06001442297f
szhshp <24031shp@sina.com>
2019-03-06 23:11:19 +0800

    Updated: V0.3

--- TickTickMarkdown.user.js
+++ TickTickMarkdown.user.js
@@ -1,7 +1,10 @@
+// TODO: add styles, add hotkeys
+
+
 // ==UserScript==
 // @name         TickTickMarkdown
 // @namespace    https://develcraft.com/
-// @version      0.2
+// @version      0.3
 // @description  Add markdown support to TickTick
 // @author       szhshp
 // @match        https://dida365.com/*
@@ -12,197 +15,166 @@
 // @run-at       document-body
 // ==/UserScript==
 
-(function() {
-    'use strict';
+(function () {
+  'use strict';
 
-    // CSS
-    const css = `
-        .editor-toggle {
-            padding-left: 10px;
-        }
+  // CSS
+  const css = `
+.editor-toggle {
+padding-left: 10px;
+}
+.editor-toggle a {
+color: #333;
+}
+.rendered-markdown {
+padding-bottom: 2em;
+}
+.rendered-markdown * {
+line-height: 1.6;
+-moz-user-select: text;
+-webkit-user-select: text;
+}
+.rendered-markdown li, .rendered-markdown ol, .rendered-markdown ul {
+list-style: disc;
+}
+.rendered-markdown ol, .rendered-markdown ul {
+padding-left: 2em;
+}
+.rendered-markdown li p {
+padding: 0
+}
+.rendered-markdown code {
+color: blue;
+}
+.rendered-markdown blockquote {
+border-left: grey solid 5px;
+padding-left: 1rem;
+margin-inline-start: 1rem;
+background: lightgrey;
+}
+.rendered-markdown p {
+padding-top: 0.5em;
+padding-bottom: 0.5em;
+}
+.rendered-markdown h3, .rendered-markdown h4, .rendered-markdown h5, .rendered-markdown h6 {
+color: #000;
+line-height: 1.8;
+}
+.rendered-markdown pre {
+font-family: monospace;
+border: 1px solid #ddd;
+background-color: #f4f4f4;
+padding: 0.5em;
+overflow: auto;
+}
+@media screen and (min-width: 800px) {
+div.g-center {
+right: 46%;
+}
+div#right-view {
+width: 46%;
+font-size: 60%!important;
+}
+}
+.priority-low:not(.checked) {
+background: #17ffa433;
+}
+.priority-medium:not(.checked) {
+background: #ffc81752;
+}
+.priority-high:not(.checked) {
+background: #f144543b;
+}
+.task.selected {
+border: 2px solid grey;
+}
+.task.active, .task.selected:hover {
+border: 1px solid grey;
+}
+#md-btn {
+  z-index: 1000;
+  border-radius: 5px;
+  padding: 2px;
+}
+`;
+  GM_addStyle(css);
 
-        .editor-toggle a {
-            color: #333;
-        }
+  // Make sure jquery isn't conflicting with anything
+  const jq = $.noConflict();
 
-        .rendered-markdown {
-            padding-bottom: 2em;
-        }
+  // Initialize showdown (markdown renderer)
+  // Docs: https://github.com/showdownjs/showdown/wiki/Showdown-options
+  const converter = new showdown.Converter({
+    simplifiedAutoLink: true,
+    excludeTrailingPunctuationFromURLs: true,
+    tables: true,
+    strikethrough: true,
+    ghCodeBlocks: true,
+    tasklists: true,
+    disableForced4SpacesIndentedSublists: true,
+    simpleLineBreaks: true,
+    noHeaderId: true,
+    headerLevelStart: 3,
+  });
 
-        .rendered-markdown * {
-            line-height: 1.6;
+  const addMarkdownToggle = () => {
+    jq('#td-md').remove()
+    jq('#td-caption').after(`
+    <div id="td-md">
+      <button id="md-btn" class="btn-sml line-right" data-mode="edit">Preview</button>
+    </div>
+    `);
 
-            -moz-user-select: text;
-            -webkit-user-select: text;
-        }
+  }
 
-        .rendered-markdown li,
-        .rendered-markdown ol,
-        .rendered-markdown ul {
-            list-style: disc;
-        }
 
-        .rendered-markdown ol,
-        .rendered-markdown ul {
-            padding-left: 2em;
-        }
+  addMarkdownToggle();
+  jq('body').on('click', '#md-btn', e => {
+    let md_mode = jq(e.target).data('mode');
+    let md_editor = jq('#taskcontent .MDEditor');
+    let md_previewer = jq('#taskcontent .MDPreview');
 
-        .rendered-markdown li p {
-            padding: 0
-        }
+    if (md_mode == 'edit') {
+      /* change to preview mode */
+      let md_text = jq('.CodeMirror-line').map((i, e) => jq(e).text()).toArray().join('\r\n');
+      let md_html = converter.makeHtml(md_text);
 
-        .rendered-markdown code {
-            color: blue;
-        }
+      md_editor.hide();
+      md_previewer.show()
 
-        .rendered-markdown blockquote {
-            border-left: grey solid 5px;
-            padding-left: 1rem;
-            margin-inline-start: 1rem;
-            background: lightgrey;
-        }
+      if (md_previewer.length > 0) {
+        md_previewer.html(md_html);
+      } else {
+        md_editor.after(`<div class="MDPreview"></div>`);
+        md_previewer = jq('#taskcontent .MDPreview');
+        md_previewer.html(md_html)
+      }
 
-        .rendered-markdown p {
-            padding-top: 0.5em;
-            padding-bottom: 0.5em;
-        }
+      jq(e.target).data('mode', 'preview');
+      jq(e.target).html('Edit')
+    } else {
+      /* change to edit mode */
+      md_editor.show();
+      md_previewer.hide()
 
-        .rendered-markdown h3,
-        .rendered-markdown h4,
-        .rendered-markdown h5,
-        .rendered-markdown h6 {
-            color: #000;
-            line-height: 1.8;
-        }
-
-        .rendered-markdown pre {
-            font-family: monospace;
-            border: 1px solid #ddd;
-            background-color: #f4f4f4;
-            padding: 0.5em;
-            overflow: auto;
-        }
-
-        @media screen and (min-width: 800px) {
-          div.g-center {
-            right: 46%;
-          }
-
-          div#right-view {
-            width: 46%;
-            font-size: 60%!important;
-          }
-        }
-
-        .priority-low:not(.checked)  {
-            background: #17ffa433;
-        }
-
-        .priority-medium:not(.checked)  {
-            background: #ffc81752;
-        }
-
-        .priority-high:not(.checked)  {
-            background: #f144543b;
-        }
-
-        .task.selected {
-            border: 2px solid grey;
-        }
-
-        .task.active, .task.selected:hover {
-            border: 1px solid grey;
-        }
-    `;
-    GM_addStyle(css);
-
-    // Make sure jquery isn't conflicting with anything
-    const jq = $.noConflict();
-
-    // console.log('Hello, I\'m TickTickMarkdown!');
-    // console.log('This is Showdown:', showdown);
-    // console.log('This is jQuery:', jq);
-
-    // Initialize showdown (markdown renderer)
-    // Docs: https://github.com/showdownjs/showdown/wiki/Showdown-options
-    const converter = new showdown.Converter({
-        simplifiedAutoLink: true,
-        excludeTrailingPunctuationFromURLs: true,
-        tables: true,
-        strikethrough: true,
-        ghCodeBlocks: true,
-        tasklists: true,
-        disableForced4SpacesIndentedSublists: true,
-        simpleLineBreaks: true,
-        noHeaderId: true,
-        headerLevelStart: 3,
-    });
-
-    // Select the node that will be observed for mutations
-    const targetNode = jq('body').get(0);
-
-    // Options for the observer (which mutations to observe)
-    const observerConfig = { childList: true, subtree: true };
-
-    const func_ToggleEdit = function(){
-
+      jq(e.target).data('mode', 'edit');
+      jq(e.target).html('Preview')
     }
 
-    // Callback function to execute when mutations are observed
-    const callback = function(mutationsList) {
-        for(const mutation of mutationsList) {
-            const taskContentNode = jq('#taskcontent');
-            var renderedMarkdownNode = jq('.rendered-markdown', taskContentNode);
-            const editorNode = jq('.editor-with-link', taskContentNode);
-            const toolbarNode = jq('#td-caption');
-            var editorToggleNode = jq('.editor-toggle', toolbarNode);
+  })
 
-            // Add the rendered markdown
-            if (editorNode.get(0) !== undefined && renderedMarkdownNode.get(0) === undefined) {
-                // Hide the editor
-                editorNode.hide();
+  // if change to another task
+  jq('body').on('click', '.l-task', e => {
+    jq('.MDPreview').hide();
+    jq('.MDEditor').show();
+    addMarkdownToggle();
+  })
 
-                // Show the rendered markdown node
-                const text = editorNode.text();
-                const html = converter.makeHtml(text);
-                renderedMarkdownNode = jq(jq.parseHTML('<div class="rendered-markdown"></div>'));
-                renderedMarkdownNode.html(html);
-                taskContentNode.append(renderedMarkdownNode);
-            }
+  jq(document).on('keydown', null, 'f4', e => {
+    jq('#md-btn').trigger('click');
+  });
 
-            // Render the editor-toggle button and register the click event
-            if (editorNode.get(0) !== undefined && editorToggleNode.get(0) === undefined) {
-                editorToggleNode = jq(jq.parseHTML('<div class="editor-toggle line-right"><a>&#x1f589;</a></div>'));
-                toolbarNode.prepend(editorToggleNode);
 
-                jq(document).on('keydown', null, 'f4',function(e) {
-                    const editorNode = jq('.editor-with-link', taskContentNode);
-                    const renderedMarkdownNode = jq('.rendered-markdown', taskContentNode);
-                    if (editorNode.is(':visible')) {
-                        editorNode.hide();
-                        renderedMarkdownNode.show();
-                        jq('a', editorToggleNode).html('&#x1f589;'); // Unicode Character 'LOWER LEFT PENCIL' (U+1F589)
-
-                        // Remove the rendered markdown node to trigger its update
-                        renderedMarkdownNode.remove();
-                    } else {
-                        editorNode.show();
-                        renderedMarkdownNode.hide();
-                        jq('a', editorToggleNode).html('&#x1f441;'); // Unicode Character 'EYE' (U+1F441)
-                    }
-                });
-            }
-        }
-    };
-
-    // Create an observer instance linked to the callback function
-    const observer = new MutationObserver(callback);
-
-    // Start observing the target node for configured mutations
-    observer.observe(targetNode, observerConfig);
-
-    // Later, you can stop observing
-    //observer.disconnect();
 
 
 })();
